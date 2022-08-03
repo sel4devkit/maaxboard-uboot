93f9df7cfd4c324e4d15a06aff2170e6f8d8d2b5
jdselby_cap <joshua.selby@capgemini.com>
2022-08-03 09:28:13 +0100

    change clone.sh to point at avnet repos on specific commits

--- clone.sh
+++ clone.sh
@@ -11,15 +11,24 @@
 
 # Clone the u-boot source
 echo "Cloning U-Boot source code..."
-git clone https://github.com/sel4devkit/uboot-imx
+git clone https://github.com/Avnet/uboot-imx.git
+cd uboot-imx 
+git checkout f752480a4c29d3ff7fd4008863adc286019bf594
+cd ..
 
 # Clone ARM trusted firmware
 echo "Cloning ARM Trusted Firmware..."
-git clone https://github.com/sel4devkit/imx-atf
+git clone https://github.com/Avnet/imx-atf.git
+cd imx-atf
+git checkout d801fd97ea9606bf5a686334639abac2ddb77985
+cd ..
 
 # Clone the imx-mkimage tools
 echo "Cloning imx-mkimage tools..."
-git clone https://github.com/sel4devkit/imx-mkimage
+git clone https://github.com/Avnet/imx-mkimage.git
+cd imx-mkimage
+git checkout 9a299d31a2045db3eafb7ee61ec3c7ee87225d53
+cd ..
 
 # Provide some guidance on usage
 echo "+----------------------------------------------------------------+"
